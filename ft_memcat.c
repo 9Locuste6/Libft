77f50adb4e28127299ed6025bf403a41ca010123
Grégoire Orban <gregoire@MacBook-Pro-de-Gregoire.local>
2020-11-02 17:27:07 +0100

    ft_memcat: move free at the end and free whene error

--- ft_memcat.c
+++ ft_memcat.c
@@ -18,19 +18,22 @@ char	*ft_memcat(char *string, char *buf, int string_size, int buf_size)
 	char	*string2;
 
 	if (!(string2 = malloc(sizeof(char) * (string_size + buf_size + 1))))
+	{
+		free(string);
 		return (NULL);
+	}
 	i = 0;
 	while (i < string_size)
 	{
 		string2[i] = string[i];
 		i++;
 	}
-	free(string);
 	while (i < string_size + buf_size)
 	{
 		string2[i] = buf[i - string_size];
 		i++;
 	}
+	free(string);
 	string2[i] = '\0';
 	return (string2);
 }
